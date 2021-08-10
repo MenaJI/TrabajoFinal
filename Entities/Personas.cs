0145503413a5f07bcfecb89e98d73f654b5dc067
Jose Menapace <jose.menapace@incluit.com>
2021-08-09 22:13:26 -0300

    Se agrego el .gitignore

--- Entities/Personas.cs
+++ Entities/Personas.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ApiREST.Entities
@@ -9,24 +10,23 @@ namespace ApiREST.Entities
         // public Usuario usuario {get; set;}
         public string Nombre { get; set; }
         public string Apellido { get; set; }
+
         public int Fk_TipoDoc { get; set; }
-        [ForeignKey("Fk_TipoDoc")]
-        public TiposDocs TipoDoc { get; set; }
+        [ForeignKey("Fk_TipoDoc")] public TiposDocs TipoDoc { get; set; }
+
         public double NroDocumento { get; set; }
+
         public int Fk_Genero { get; set; }
-        [ForeignKey("Fk_Genero")]
-        public Generos Genero { get; set; }
+        [ForeignKey("Fk_Genero")] public Generos Genero { get; set; }
+
         public int Fk_Localidad { get; set; }
-        [ForeignKey("Fk_Localidad")]
-        public Localidades Localidad { get; set; }
+        [ForeignKey("Fk_Localidad")] public Localidades Localidad { get; set; }
+
         public int Fk_Nacionalidad { get; set; }
-        [ForeignKey("Fk_Nacionalidad")]
-        public Nacionalidades Nacionalidad { get; set; }
+        [ForeignKey("Fk_Nacionalidad")] public Nacionalidades Nacionalidad { get; set; }
         public int Fk_EstadoCivil { get; set; }
-        [ForeignKey("Fk_EstadoCivil")]
-        public EstadosCiviles EstadoCivil { get; set; }
+        [ForeignKey("Fk_EstadoCivil")] public EstadosCiviles EstadoCivil { get; set; }
         public string Fk_Usuario { get; set; }
-        [ForeignKey("Fk_Usuario")]
-        public Usuarios Usuario { get; set; }
+        [ForeignKey("Fk_Usuario")] public Usuarios Usuario { get; set; }
     }
 }
