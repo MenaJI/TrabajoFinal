173d7236ad484e88103063b4a7de0540f620c05a
Jose Menapace <jose.menapace@incluit.com>
2022-01-23 21:00:04 -0300

    Correccion en el mensaje de retorno para el metodo ChangeItem EC

--- Controllers/EstadosCivilesController.cs
+++ Controllers/EstadosCivilesController.cs
@@ -43,7 +43,7 @@ namespace ApiREST.Controllers
         {
             estadosCivilesServices.Update(estadoCivil);
 
-            return NotFound();
+            return Ok();
         }
 
         [HttpDelete("RemoveItem")]
